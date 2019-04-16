ce62a4c746096c9a4250a14a5cc31374fb2e7a0e
spacetiller <zhanghui@pk4yo.com>
2019-04-16 20:39:32 +0800

    add question and answer

--- pages/ask/ask.js
+++ pages/ask/ask.js
@@ -1,66 +1,97 @@
 // pages/ask/ask.js
+var Mcaptcha = require('../../utils/mcaptcha.js');
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		title: "",
+		imgCode: "",
+		cvs: {
+			width: 160,
+			height: 70
+		},
+		content: "",
+		labels: "",
+		allabels: ["买房", "卖房", "交易过户", "贷款", "税收", "其他"]
 	},
+
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		
+	},
+	/**生成字母数组**/
+	getAllLetter: function () {
+		var letterStr = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+		return letterStr.split(",");
+	},
+	getAllLetterNum: function () {
+		var letterStr = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+		return letterStr.split(",");
+	},
+	randomNum: function (min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
+	},
+	createCode: function(len) {
+		var letterArr = this.getAllLetterNum();
+		var code = '';
+		for(var i = 0; i<len; i++){
+			
+			code += letterArr[this.randomNum(0, letterArr.length)];
+		}
+		return code;
+	},
 
+	inputVerify: function (e) {
+		var code = e.detail.value;
+		this.setData({
+			imgCode: code
+		});
+	},
+	submit: function () {
+		var res = this.mcaptcha.validate(this.data.imgCode);
+		if (this.data.imgCode == '' || this.data.imgCode == null) {
+			wx.showToast({
+				title: '请输入图形验证码！',
+				icon: 'none',
+				duration: 1000,
+				mask: false
+			})
+			return;
+		}
+		if (!res) {
+			wx.showToast({
+				title: '图形验证码错误！',
+				icon: 'none',
+				duration: 1000,
+				mask: false
+			})
+			return;
+		}
+	},
+
+	onTap() {
+		var code = this.createCode(4);
+		this.mcaptcha.refresh(code);
 	},
 
 	/**
 	 * 生命周期函数--监听页面初次渲染完成
 	 */
 	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+		wx.setNavigationBarTitle({
+			title: "提问"
+		});
+		var code = this.createCode(4);
+		this.mcaptcha = new Mcaptcha({
+			el: 'canvas',
+			width: this.data.cvs.width/3,
+			height: this.data.cvs.height/3,
+			code: code
+		});
 	}
 })
--- utils/util.js
+++ utils/util.js
@@ -14,73 +14,73 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 };
 
-const array_find_obj = function (array, key, value) {
-	var index = -1;
-	for (let i = 0; i < array.length; i++) {
-		//console.log(resultSet[i])
-		if (value == array[i][key]) {
-			index = i;
-			return index;
-		}
-	}
-	return index;
+const array_find_obj = function(array, key, value) {
+  var index = -1;
+  for (let i = 0; i < array.length; i++) {
+    //console.log(resultSet[i])
+    if (value == array[i][key]) {
+      index = i;
+      return index;
+    }
+  }
+  return index;
 };
 
-const checkMobile =  function (mobile) {
-	let reg = new RegExp(/^1\d{10}$/);
-	if (reg.test(mobile)) {
-		return true
-	} else {
-		return false
-	}
+const checkMobile = function(mobile) {
+  let reg = new RegExp(/^1\d{10}$/);
+  if (reg.test(mobile)) {
+    return true
+  } else {
+    return false
+  }
 };
 
-const myStorageFind = function (key, key2, value2) {
-	//const key = 'myfavorites';
-	var favs = wx.getStorageSync(key);
-	if (favs.length == 0) {
-		console.log("Info: my storage empty!!!");
-		return;
-	}
-	//var pid = this.data.project.pId;
-	var fi = array_find_obj(favs, key2, value2);
-	return fi;
-
-};
-
-const myStorageDel = function () {
-	//const key = 'myfavorites';
-	var favs = wx.getStorageSync(key);
-	if (favs.length == 0) {
-		console.log("Info: my storage empty!!!");
-		return;
-	}
-	//var pid = this.data.project.pId;
-	var fi = array_find_obj(favs, key2, value2);
-	//console.log(fi);
-	favs.splice(fi, 1);
-	wx.setStorage({
-		key: key,
-		data: favs
-	});
+const myStorageFind = function(key, key2, value2) {
+  //const key = 'myfavorites';
+  var favs = wx.getStorageSync(key);
+  if (favs.length == 0) {
+    console.log("Info: my storage empty!!!");
+    return;
+  }
+  //var pid = this.data.project.pId;
+  var fi = array_find_obj(favs, key2, value2);
+  return fi;
 
 };
 
-if  (!Array.indexOf)  {
-	    Array.prototype.indexOf  =  function  (obj)  {
-		        for  (var  i  =  0;  i  <  this.length;  i++)  {
-			            if  (this[i]  ==  obj)  {
-				                return  i;
-			            }
-		        }
-		        return  -1;
-	    }
+const myStorageDel = function() {
+  //const key = 'myfavorites';
+  var favs = wx.getStorageSync(key);
+  if (favs.length == 0) {
+    console.log("Info: my storage empty!!!");
+    return;
+  }
+  //var pid = this.data.project.pId;
+  var fi = array_find_obj(favs, key2, value2);
+  //console.log(fi);
+  favs.splice(fi, 1);
+  wx.setStorage({
+    key: key,
+    data: favs
+  });
+
+};
+
+if  (!Array.indexOf)  {    
+  Array.prototype.indexOf  =   function (obj)  {        
+    for  (var  i  =  0;  i  <  this.length;  i++)  {            
+      if  (this[i]  ==  obj)  {                
+        return  i;            
+      }        
+    }        
+    return  -1;    
+  }
 }
 
 module.exports = {
   formatTime: formatTime,
-	array_find_obj: array_find_obj,
-	myStorageFind: myStorageFind,
-	myStorageDel: myStorageDel,
-	checkMobile: checkMobile
+  array_find_obj: array_find_obj,
+  myStorageFind: myStorageFind,
+  myStorageDel: myStorageDel,
+  checkMobile: checkMobile
 }
